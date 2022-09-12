8138126e4dda62355140c01f2d57b3116518019c
Multimide <ajibolaladipo1@gmail.com>
2022-09-12 03:13:02 -0700

    Types of numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,43 +1,32 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
-/* more headers goes there */
-
-
-
-/* betty style doc for function main goes there */
+/*
+ *main- This is a C programm that prints if a
+ *number is positive or negative
+ *
+ *Return: Always 0 (sucess)
+ */
 
 int main(void)
-
 {
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
-		int n;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
 
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
 
-
-			srand(time(0));
-
-				n = rand() - RAND_MAX / 2;
-
-				if (n > 0)
-				{
-				
-					printf("%d is positive\n", n)
-				}
-
-				else if (n = 0)
-				{
-					printf("%d is zero\n", n)
-	
-				}
-
-				else
-				{
-					printf("%d is negative\n",n)
-				}
-
-					return (0);
-
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
 }
