acd8898a039c3a66d80414a7223051765e1040f4
Multimide <ajibolaladipo1@gmail.com>
2022-09-12 17:45:09 -0700

    using the putchar function

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- This is a C program that
+ *prints the alphabets in lower case
+ *
+ *Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	char a = 'a';
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if ((a != 'e') && (a != 'q'))
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
