d571ed8b99645a9326180a64d1847970b79ea430
Multimide <ajibolaladipo1@gmail.com>
2022-09-12 20:41:53 -0700

    Printing combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main -Print the combination of two digits
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int tens, ones;
+
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = tens + 1; ones <= 9; ones++)
+		{
+			putchar(tens + '0');
+			putchar(ones + '0');
+
+			if (tens < 0)
+			{
+				putchar(',');
+				putchar(' ');
+
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
