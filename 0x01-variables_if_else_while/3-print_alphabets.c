9fec2f7470ec6165c40b52237cbee429afa815a0
Multimide <ajibolaladipo1@gmail.com>
2022-09-12 21:00:16 -0700

    Corrections

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 /**
  * main -This is a C program that prints a code in upper
- * and lower casw
+ * and lower case
  * Return: Always 0 (Sucess)
  */
 
 int main(void)
 {
 	char a, b;
-	a = 'a';
 
+	a = 'a';
 	b = 'A';
 
 	for (a = 'a'; a <= 'z'; a++)
 	{
 		putchar(a);
 	}
+
 	for (b = 'A'; b <= 'Z'; b++)
 	{
 		putchar(b);
