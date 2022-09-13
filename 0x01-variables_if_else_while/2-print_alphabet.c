ca989a38814045af9dfbf0a119ab146e8c852d06
Multimide <ajibolaladipo1@gmail.com>
2022-09-12 20:55:05 -0700

    Correction

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,14 +10,13 @@
 int main(void)
 {
 	char a = 'a';
+
 	for (a = 'a'; a <= 'z'; a++)
 	{
-		if ((a != 'e') && (a != 'q'))
-		{
-			putchar(a);
-		}
+		putchar(a);
 	}
-	putchar('\n');
+	putchar(
+			'\n');
 
 	return (0);
 }
