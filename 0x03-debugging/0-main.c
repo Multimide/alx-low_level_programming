5967fb107b5ade6b949110b86f382f31905e862f
Multimide <ajibolaladipo1@gmail.com>
2022-09-16 06:04:18 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main- debugging
+ * main - tests function that prints if integer is positive or negative
  *
  * Return: 0
  */
@@ -11,6 +10,8 @@ int main(void)
 {
 	int i;
 
+	i = 0;
 	positive_or_negative(i);
+
 	return (0);
 }
