f93388928e655869320f67b494451fa1f368e369
Multimide <ajibolaladipo1@gmail.com>
2022-09-19 13:40:40 -0700

    Changing variables with pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - take a pointer to an integer as
+ * parameter and update its value
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+	printf("*n\n");
+}
