04b0020ab64d556323b10733c4db770c9bdb843c
Multimide <ajibolaladipo1@gmail.com>
2022-09-20 10:31:58 -0700

    conversion

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *_strcpy- copy string
+ * @dest- copy to
+ * @*scr- copy from
+ *
+ * Return: Pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, temp, c;
+
+	c = strlen(scr);
+	for (i = 0; i < c; i++)
+	{
+		temp = scr[i];
+		dest[i] = temp;
+	}
+	return (dest)
+}
